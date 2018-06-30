d861710755ffc9b5c4a4938fbf5ed86afc5c6c7d
hsimao <e087754958@gmail.com>
2018-06-30 19:10:00 +0800

    fix chart init

--- src/components/admin/Chart.vue
+++ src/components/admin/Chart.vue
@@ -13,86 +13,82 @@ export default {
     return {
 
     }
-  }
-}
+  },
+  mounted(){
+    const days = ['1', '2', '3', '4', '5', '6', '7', '8']
 
-// chart.js
-const days = ['1', '2', '3', '4', '5', '6', '7', '8']
-// const config = ;
+    const chartEl = document.getElementsByClassName('chart')[0]
+    const canvas = document.getElementById("myChart")
+    const ctx = canvas.getContext('2d')
+    let chartW = canvas.width = chartEl.clientWidth
+    let chartH = canvas.height = chartEl.clientHeight
 
-window.onload = function(){
-  const chartEl = document.getElementsByClassName('chart')[0]
-  const canvas = document.getElementById("myChart")
-  const ctx = canvas.getContext('2d')
-  let chartW = canvas.width = chartEl.clientWidth
-  let chartH = canvas.height = chartEl.clientHeight
+    var bg1 = ctx.createLinearGradient(0,0,0,chartW);
+    bg1.addColorStop(0,'rgba(172, 92, 250, 0.9)');
+    bg1.addColorStop(0.5,'rgba(172, 92, 250, 0.7)');
+    bg1.addColorStop(1,'rgba(172, 92, 250, 0.7)');
 
-  var bg1 = ctx.createLinearGradient(0,0,0,chartW);
-  bg1.addColorStop(0,'rgba(172, 92, 250, 0.9)');
-  bg1.addColorStop(0.5,'rgba(172, 92, 250, 0.7)');
-  bg1.addColorStop(1,'rgba(172, 92, 250, 0.7)');
+    var bg2 = ctx.createLinearGradient(0,0,0,chartW);
+    bg2.addColorStop(0,'rgba(52, 225, 202, 0.9)');
+    bg2.addColorStop(0.5,'rgba(52, 225, 202, 0.7)');
+    bg2.addColorStop(1,'rgba(52, 225, 202, 0.7)');
 
-  var bg2 = ctx.createLinearGradient(0,0,0,chartW);
-  bg2.addColorStop(0,'rgba(52, 225, 202, 0.9)');
-  bg2.addColorStop(0.5,'rgba(52, 225, 202, 0.7)');
-  bg2.addColorStop(1,'rgba(52, 225, 202, 0.7)');
-
-  const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['1', '2', '3', '4', '5', '6', '7', '8'],
-      datasets: [{
-        label: 'FIRST PRODUCTS',
-        borderWidth: '0',
-        backgroundColor: bg1,
-        data: [0, 20, 10, 45, 20, 35, 20, 10]
-      },{
-        label: 'SECOND PRODUCTS',
-        borderWidth: '0',
-        backgroundColor: bg2,
-        data: [2, 10, 10, 20, 20, 15, 10, 15]
-      }]
-    },
-    options: {
-      layout: {
-        padding: {
-          left: 30,
-          right: 30,
-          top: 30,
-          bottom: 30
-        }
-      },
-      responsive: true,
-      tooltips: {
-        mode: 'index'
-      },
-      hover: {
-        mode: 'index'
-      },
-      scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Days'
-          }
-        }],
-        yAxes: [{
-          stacked: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Value'
-          }
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8'],
+        datasets: [{
+          label: 'FIRST PRODUCTS',
+          borderWidth: '0',
+          backgroundColor: bg1,
+          data: [0, 20, 10, 45, 20, 35, 20, 10]
+        },{
+          label: 'SECOND PRODUCTS',
+          borderWidth: '0',
+          backgroundColor: bg2,
+          data: [2, 10, 10, 20, 20, 15, 10, 15]
         }]
+      },
+      options: {
+        layout: {
+          padding: {
+            left: 30,
+            right: 30,
+            top: 30,
+            bottom: 30
+          }
+        },
+        responsive: true,
+        tooltips: {
+          mode: 'index'
+        },
+        hover: {
+          mode: 'index'
+        },
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Days'
+            }
+          }],
+          yAxes: [{
+            stacked: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Value'
+            }
+          }]
+        }
       }
-    }
-  });
+    });
 
-  window.onresize = function(){
-    chartW = canvas.width = chartEl.clientWidth
-    chartH = canvas.height = chartEl.clientHeight
+    window.onresize = function(){
+      chartW = canvas.width = chartEl.clientWidth
+      chartH = canvas.height = chartEl.clientHeight
+    }
   }
 }
-
 
 </script>
 
